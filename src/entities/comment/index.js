f8299fb69eed71943c0c4b8c024f2cf769ca07db
ando <dokyan1989@gmail.com>
2019-12-17 16:28:00 +0700

    add comment entity

---
+++ src/entities/comment/index.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+const Id = require('../../helpers/Id');
+const ipRegex = require('ip-regex');
+const sanitizeHtml = require('sanitize-html');
+const buildMakeComment = require('./comment');
+const buildMakeSource = require('./source');
+
+const makeSource = buildMakeSource({ isValidIp });
+const makeComment = buildMakeComment({ Id, md5, sanitize, makeSource });
+
+module.exports = makeComment;
+
+function isValidIp(ip) {
+  return ipRegex({ exact: true }).test(ip);
+}
+
+function md5(text) {
+  return crypto
+    .createHash('md5')
+    .update(text, 'utf-8')
+    .digest('hex');
+}
+
+function sanitize(text) {
+  return sanitizeHtml(text, {
+    allowedIframeHostnames: ['codesandbox.io', 'repl.it']
+  });
+}
